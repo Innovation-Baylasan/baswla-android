784047c5e9ea7d7778d447c7c3078c315a2913ab
m7mdra <xm7mdrax@gmail.com>
2019-11-25 13:49:45 +0200

    added map layer design

---
+++ app/src/main/java/org/baylasan/sudanmap/MapLayersAdapter.kt
@@ -0,0 +1,53 @@
+package org.baylasan.sudanmap
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class MapLayersAdapter : RecyclerView.Adapter<MapLayersViewHolder>() {
+    val list =
+        mutableListOf(
+            Selectable(item = "item1"),
+            Selectable(item = "item2"),
+            Selectable(item = "item3"),
+            Selectable(item = "item4"),
+            Selectable(item = "item5"),
+            Selectable(item = "item6"),
+            Selectable(item = "item7")
+        )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MapLayersViewHolder {
+        return MapLayersViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.row_map_layer,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: MapLayersViewHolder, position: Int) {
+        holder.apply {
+            val selectable = list[adapterPosition]
+            layerName.text = selectable.item
+            itemView.setOnClickListener {
+                selectable.toggle()
+                notifyItemChanged(adapterPosition)
+            }
+            layerSelected.setColorFilter(
+
+                ContextCompat.getColor(
+                    layerSelected.context,
+                    if (selectable.isSelected) R.color.colorAccent else android.R.color.darker_gray
+                )
+            )
+            layerCard.strokeColor=  ContextCompat.getColor(
+                layerSelected.context,
+                if (selectable.isSelected) R.color.colorAccent else android.R.color.transparent
+            )
+        }
+    }
+}
---
+++ app/src/main/java/org/baylasan/sudanmap/MapLayersFragment.kt
@@ -0,0 +1,42 @@
+package org.baylasan.sudanmap
+
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_map_layers.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MapLayersFragment : Fragment() {
+    private lateinit var activity: Activity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = context as Activity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_map_layers,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapLayersRecyclerView.layoutManager = GridLayoutManager(activity, 3)
+
+        mapLayersRecyclerView.adapter = MapLayersAdapter()
+    }
+
+}
