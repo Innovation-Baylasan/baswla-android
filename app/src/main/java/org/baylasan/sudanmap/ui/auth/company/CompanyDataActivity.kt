0e35a2ce013f09f41bbed296f50d09fbb865fad7
kldmohammed <kldmohammed@outlook.com>
2020-01-15 11:47:10 +0200

    completing company information screen
    Took 1 hour 6 minutes

---
+++ app/src/main/java/org/baylasan/sudanmap/ui/auth/company/CompanyDataActivity.kt
@@ -0,0 +1,13 @@
+package org.baylasan.sudanmap.ui.auth.company
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.baylasan.sudanmap.R
+
+class CompanyDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_data)
+    }
+}
